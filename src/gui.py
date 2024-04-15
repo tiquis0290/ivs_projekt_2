af94b9169178932d45478b6524062ce2ebc9c77d
Marek Maslowski <marek1.maslowski@gmail.com>
2024-04-15 16:02:25 +0200

    connecting gui to math.lib

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from logic import calculate
+import libs.calc_mathlib as math
 
 def info():
     root2 = tk.Tk()
@@ -14,21 +14,118 @@ def info():
 root = tk.Tk()
 root.title("Calculator")
 
+x = ""
+operation = ""
+y = ""
+
 
 text = tk.StringVar()
-text.set("")
+
+def setText():
+    global x
+    global y
+    global operation
+    if operation == "√":
+        text.set(str(y) + "|" + str(operation) + str(x))
+    else:
+        text.set(str(x) + str(operation) + str(y) + "|")
+
 
 def addchar(char):
-    text.set(text.get() + char)
+    global x
+    global y
+    global operation
+    if char != "." or "." not in y:
+        y += char
+        setText()
 
-def c():
-    text.set(text.get()[:-1])
+def twoOperation(char):
+    global x
+    global y
+    global operation
+    if operation != "":
+        operation = char
+        solve()
+    else:
+        x = float(y)
+        if int(x) == x:
+            x = int(x)
+        y = ""
+        operation = char
+        setText()
 
 def ce():
-    text.set("")
+    global x
+    global y
+    global operation
+    y = y[:-1]
+
+def c():
+    global x
+    global y
+    global operation
+    x = ""
+    y = ""
+    operation = ""
+    setText()
+
+def oneOperation(char):
+    global x 
+    global y
+    global operation
+    x = float(y)
+    if int(x) == x:
+        x = int(x)
+    y = 0
+    operation = char
+    solve()
 
 def solve():
-    text.set(calculate(text.get()))
+    global x
+    global y
+    global operation
+    y = float(y)
+    if int(y) == y:
+        y = int(y)
+    print(x, operation, y)
+    try:
+        if operation == "+":
+            result = math.add(x, y)
+        elif operation == "-":
+            result = math.sub(x, y)
+        elif operation == "*":
+            result = math.mul(x, y)
+        elif operation == "/":
+            result = math.div(x, y)
+        elif operation == "^":
+            result = math.pow(x, y)
+        elif operation == "√":
+            result = math.root(x,y)
+        elif operation == "|":
+            result = math.abs(x)
+        elif operation == "!":
+            if isinstance(x, int):
+                result = math.fac(x)
+            else:
+                c()
+                text.set("Error: number must be INT")
+        else:
+            result = y
+        print(result)
+        x = ""
+        operation = ""
+        y = str(result)
+        setText()
+    except:
+        c()
+        text.set("Error: syntax error")
+
+
+
+setText()
+
+
+        
 
 button = tk.Button(root, text="Information", font=("Arial", 10), padx=20, pady=10, command=info)
 button.grid(row=0, column=0, columnspan=4, padx=5, pady=5, sticky="nsew")
@@ -42,22 +139,22 @@ button.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
 button = tk.Button(root, text="C", font=("Arial", 18), padx=20, pady=10, command=c)
 button.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="^", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('^'))
+button = tk.Button(root, text="^", font=("Arial", 18), padx=20, pady=10, command=lambda: twoOperation('^'))
 button.grid(row=2, column=2, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="√", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('√('))
+button = tk.Button(root, text="y√x", font=("Arial", 18), padx=20, pady=10, command=lambda: twoOperation('√'))
 button.grid(row=2, column=3, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="(", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('('))
+button = tk.Button(root, text="!", font=("Arial", 18), padx=20, pady=10, command=lambda: oneOperation('!'))
 button.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text=")", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar(')'))
+button = tk.Button(root, text=")", font=("Arial", 18), padx=20, pady=10, command=lambda: oneOperation(')'))
 button.grid(row=3, column=1, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="/", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('/'))
+button = tk.Button(root, text="/", font=("Arial", 18), padx=20, pady=10, command=lambda: twoOperation('/'))
 button.grid(row=3, column=2, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="*", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('*'))
+button = tk.Button(root, text="*", font=("Arial", 18), padx=20, pady=10, command=lambda: twoOperation('*'))
 button.grid(row=3, column=3, padx=5, pady=5, sticky="nsew")
 
 button = tk.Button(root, text="7", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('7'))
@@ -69,7 +166,7 @@ button.grid(row=4, column=1, padx=5, pady=5, sticky="nsew")
 button = tk.Button(root, text="9", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('9'))
 button.grid(row=4, column=2, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="-", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('-'))
+button = tk.Button(root, text="-", font=("Arial", 18), padx=20, pady=10, command=lambda: twoOperation('-'))
 button.grid(row=4, column=3, padx=5, pady=5, sticky="nsew")
 
 button = tk.Button(root, text="4", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('4'))
@@ -81,7 +178,7 @@ button.grid(row=5, column=1, padx=5, pady=5, sticky="nsew")
 button = tk.Button(root, text="6", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('6'))
 button.grid(row=5, column=2, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="+", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('+'))
+button = tk.Button(root, text="+", font=("Arial", 18), padx=20, pady=10, command=lambda: twoOperation('+'))
 button.grid(row=5, column=3, padx=5, pady=5, sticky="nsew")
 
 button = tk.Button(root, text="1", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('1'))
@@ -96,7 +193,7 @@ button.grid(row=6, column=2, padx=5, pady=5, sticky="nsew")
 button = tk.Button(root, text="=", font=("Arial", 18), padx=20, pady=10, command=solve)
 button.grid(row=6, column=3, rowspan=2, padx=5, pady=5, sticky="nsew")
 
-button = tk.Button(root, text="|x|", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('|'))
+button = tk.Button(root, text="|x|", font=("Arial", 18), padx=20, pady=10, command=lambda: oneOperation('|'))
 button.grid(row=7, column=0, padx=5, pady=5, sticky="nsew")
 
 button = tk.Button(root, text="0", font=("Arial", 18), padx=20, pady=10, command=lambda: addchar('0'))
--- src/logic.py
+++
@@ -1,2 +0,0 @@
-def calculate(text):
-    return "result"
